6d712bf01b391126b8a1188deb00253450fbe8ee
Watheq Attrah <novaatrah@gmail.com>
2023-07-31 20:19:01 -0700

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "F", "dark brown", "intraverse", 0),
+(2,"Rowdy", 10, "F", "black", "extraverse", 0),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 0),
+(4,"Sergeant Brown", 19, "F", "Green", "Slimy", 0),
+(5,"Melissa", 13, "M", "dark brown", "aggressive", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "M", "Blue", "naive", 1),
+(8, NULL, 20, "M", "black", "goofy", 1);
